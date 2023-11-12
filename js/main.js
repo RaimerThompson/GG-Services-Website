3ddf0db220e3cf5b6d78609a1d0414b92d21f300
Raimer Thompson <rthomp22@gmail.com>
2023-11-12 18:15:28 -0500

    contact pg fixes and new animations

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) =>{
+  e.preventDefault()
+
+   // serviceID - templateID - #form - publicKey 
+  emailjs.sendForm('service_kmv559q', 'template_xnu3zqy', '#contact-form', 'Vs4d0AsUtYr3Zbvh_')
+    .then(() =>{
+  //show sent message 
+    contactMessage.textContent = 'Thanks for submitting!'
+
+  //remove message after 2 seconds
+  setTimeout(() => {
+    contactMessage.textContent = ''
+  }, 2000);
+
+  //clear input fields
+  contactForm.reset()
+  }, () =>{
+    //show error message
+    contactMessage.textContent = 'Sorry, message was not recieved ❌'
+  })
+}
+
+contactForm.addEventListener('submit', sendEmail)
