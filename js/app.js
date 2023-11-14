d9ba2b88f24d5c1135cb644cb517e2366de719b3
Raimer Thompson <rthomp22@gmail.com>
2023-11-14 00:21:46 -0500

    contact pg fixes

--- js/app.js
+++ js/app.js
@@ -6,10 +6,14 @@ let close = document.querySelector('span')
 images.forEach((img, index) => {
   img.addEventListener('click', () => {
     openModal(`/images/${index}.jpg`)
-  })
+  });
 });
 
-close.addEventListener('click', () => wrapper.style.display = 'none')
+// close.addEventListener('click', () => (wrapper.style.display = 'none'));
+
+function closeImg(){
+  wrapper.style.display = 'none'
+}
 
 function openModal(pic){
   wrapper.style.display = 'flex'
