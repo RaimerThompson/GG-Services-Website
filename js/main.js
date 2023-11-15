45331a249d155c4c93028fd8af30fd97f43f1b7f
Raimer Thompson <rthomp22@gmail.com>
2023-11-15 06:14:48 -0500

    adding mobile header

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+const menuBtn = document.querySelector('.hamburger__btn');
+const mobileNav = document.getElementById('navbar');
+let menuOpen = false;
+
+menuBtn.addEventListener('click', () => {
+  if (!menuOpen) {
+    menuBtn.classList.add('close');
+    menuOpen = true;
+    mobileNav.style.display = 'flex';
+    mobileNav.classList.add('active');
+    
+   }else {
+    menuBtn.classList.remove('close');
+    menuOpen = false;
+    mobileNav.style.display = 'flex';
+    mobileNav.classList.remove('active');
+   }
+})
+;
+
+
 const contactForm = document.getElementById('contact-form'),
       contactMessage = document.getElementById('contact-message')
 
