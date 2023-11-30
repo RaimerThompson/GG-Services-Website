1e3fba1bd8fa2add91e72ad06f740fd7089b57ec
Raimer Thompson <rthomp22@gmail.com>
2023-11-30 10:16:13 -0500

    fixes to home pg layout added photos aswell

--- js/main.js
+++ js/main.js
@@ -18,6 +18,32 @@ menuBtn.addEventListener('click', () => {
 })
 ;
 
+const slides = document.querySelector('.home__slider').children;
+let index = 0;
+
+function nextSlide(){
+  if (index == slides.length-1){
+    index = 0;
+  }else{
+    index++;
+  }
+  changeSlide();
+}
+
+function changeSlide(){
+  for (let i=0; i<slides.length; i++){
+      slides[i].classList.remove('active');
+  }
+
+  slides[index].classList.add('active');
+}
+
+function autoPlay(){
+  nextSlide();
+}
+let timer = setInterval(autoPlay, 3000)
+
+
 
 const contactForm = document.getElementById('contact-form'),
       contactMessage = document.getElementById('contact-message')
