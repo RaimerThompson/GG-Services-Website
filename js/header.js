175d20307652357459019ddab72dd15b54ae9b0d
Raimer Thompson <rthomp22@gmail.com>
2023-12-02 17:40:10 -0500

    added new header scroll functions through all pgs

---
+++ js/header.js
@@ -0,0 +1,9 @@
+document.addEventListener('scroll', () =>{
+  const header = document.querySelector('header');
+
+if (window.scrollY > 120) {
+  header.classList.add('scrolled');
+} else{
+  header.classList.remove('scrolled')
+}
+})
